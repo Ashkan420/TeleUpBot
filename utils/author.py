3198966c0646b2a9e1184f278e5c857a1c131755
Ashkan420 <ashkan.ebi2@gmail.com>
2026-02-17 21:28:58 +0330

    plug

--- utils/author.py
+++ utils/author.py
@@ -1,13 +1,24 @@
 from telegram import Update
 
+MY_CHANNEL_URL = "https://t.me/bichniga"
 
 def get_author_info(update: Update):
     user = update.effective_user
 
+    # Use the user's full name or fallback
     author_name = user.full_name if user else "Unknown User"
-    author_url = ""
+    #author_url = ""
 
-    if user and user.username:
-        author_url = f"https://t.me/{user.username}"
+    #if user and user.username:
+        #author_url = f"https://t.me/{user.username}"
+    author_url = MY_CHANNEL_URL
+
+    # Usernames that should be replaced
+    spoof_users = ["Brotherash210"]
+
+    # If sender is in spoof list, replace with channel identity
+    if user and user.username in spoof_users:
+        author_name = "ครнкคภ"
+        author_url = MY_CHANNEL_URL
 
     return author_name, author_url
